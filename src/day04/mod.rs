86c9db228373eb4b78809e127ee5ee84b5a4dc4b
standarddeviant <dave.crist@gmail.com>
2023-12-28 10:44:31 -0500

    day04, part1 refactor (still correct)

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -1,12 +1,15 @@
+use core::num;
 use std::collections::HashSet;
 
 use crate::utils::read_lines;
 use regex::{Regex, Match};
 
-pub fn run(fname: &str, winners_len: usize) {
-    let part1_answer = part1(fname, winners_len);
+pub fn run(fname: &str, win_len: usize) {
+    let part1_answer = part1(fname, win_len);
     println!("day04: part1: answer = {part1_answer}");
-    // part2(fname);
+
+    // let part2_answer = part2(fname, win_len);
+    // println!("day04: part2: answer = {part2_answer}");
 }
 
 fn parse_cards(lines: &Vec<String>) -> Vec<Vec<i32>> {
@@ -26,36 +29,55 @@ fn parse_cards(lines: &Vec<String>) -> Vec<Vec<i32>> {
     return cards;
 }
 
-pub fn part1(fname: &str, winners_len: usize) -> i32 {
+fn win_count(card: &Vec<i32>, win_len: usize) -> usize {
+    let mut num_winning_cards: usize = 0;
+    let _game = card[0];
+    let winners: Vec<i32> = card[1 .. (1+win_len)].into();
+    let players: Vec<i32> = card[(1+win_len).. ].into();
+    for player in players {
+        if winners.contains(&player) {
+            num_winning_cards += 1;
+        }
+    }
+    return num_winning_cards;
+}
+
+fn score_card(card: &Vec<i32>, win_len: usize) -> i32 {
+    let mut score: i32 = 0;
+    let num_winning_cards = win_count(&card, win_len);
+    if num_winning_cards > 0 {
+        score += 2_i32.pow(num_winning_cards as u32 -1);
+    }
+    return score;
+}
+
+pub fn part1(fname: &str, win_len: usize) -> i32 {
     let mut answer: i32 = 0;
     let lines = read_lines(fname);
     let cards = parse_cards(&lines);
     for card in cards {
-        let mut num_winning_cards: u32 = 0;
-        let _game = card[0];
-        let winners: Vec<i32> = card[1 .. (1+winners_len)].into();
-        let players: Vec<i32> = card[(1+winners_len).. ].into();
-        for player in players {
-            if winners.contains(&player) {
-                num_winning_cards += 1;
-            }
-        }
-        if num_winning_cards > 0 {
-            answer += 2_i32.pow(num_winning_cards-1);
-        }
+        answer += score_card(&card, win_len);
     }
     return answer;
 }
 
-pub fn part2(fname: &str) -> i32 {
-    let mut out: i32 = 0;
+// 310459 is too low
+pub fn part2(fname: &str, win_len: usize) -> i32 {
+    let mut answer: i32 = 0;
     let mut line_num: i32 = 1;
     let lines = read_lines(fname);
-    for line in lines {
-        println!("{line_num}--{line}");
-        line_num += 1;
+    let cards = parse_cards(&lines);
+    for ix in 0..cards.len() {
+        let card = &cards[ix];
+        let num_winning_cards = win_count(card, win_len);
+        for ix2 in ix+1 .. ix+1+num_winning_cards {
+        // for ix2 in 0..3 {
+            if ix2 < cards.len() {
+                let tmp_card = &cards[ix2];
+                answer += score_card(card, win_len);
+            }
+        }
     }
-    println!("dayXX: part2: sum = {out}");
-    return out;
+    return answer;
 }
 
